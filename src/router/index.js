ebc3485dc94230e379b45695e37d8c444b4adc8b
WayneLu <wayne23123@gmail.com>
2023-08-16 16:02:14 +0800

    3-4頁面配置

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import JsPageThree from "../views/jspage/JsPageThree.vue";
 import JsPageThreeOne from "../views/jspage/JsPageThreeOne.vue";
 import JsPageThreeTwo from "../views/jspage/JsPageThreeTwo.vue";
 import JsPageThreeThree from "../views/jspage/JsPageThreeThree.vue";
+import JsPageThreeFour from "../views/jspage/JsPageThreeFour.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -81,6 +82,10 @@ const router = createRouter({
                 {
                   path: "destructuringassignment",
                   component: JsPageThreeThree,
+                },
+                {
+                  path: "method",
+                  component: JsPageThreeFour,
                 },
               ],
             },
