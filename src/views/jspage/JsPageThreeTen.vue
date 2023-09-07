aebc5eea05cf165e9add761142254c7aa0a20a23
WayneLu <wayne23123@gmail.com>
2023-09-07 13:14:01 +0800

    js3-10頁面更新

--- src/views/jspage/JsPageThreeTen.vue
+++ src/views/jspage/JsPageThreeTen.vue
@@ -51,6 +51,330 @@ function locate(idPound) {
       <div class="mainContainer">
         <div class="mainLayout">
           <!-- 這裡 -->
+          <div class="articleCardTitle cor36 fz34">set</div>
+          <div class="bgcVS">
+            <p>先 new 一個 Set</p>
+            <div>
+              <span class="function">const </span>
+              <span class="word">set</span> <span class="then">= new </span>
+              <span class="word">Set</span> <span class="brackets">()</span>
+            </div>
+            <br />
+            <div><span class="comment">//往裡面放東西用.add()</span></div>
+            <div>
+              <span class="variable">set</span>
+              <span class="variable">.add</span>
+              <span class="brackets">(</span> <span class="number">1</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="variable">set</span>
+              <span class="variable">.add</span>
+              <span class="brackets">(</span> <span class="number">2</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="variable">set</span>
+              <span class="variable">.add</span>
+              <span class="brackets">(</span> <span class="number">3</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="variable">set</span>
+              <span class="variable">.add</span>
+              <span class="brackets">(</span> <span class="number">7</span>
+              <span class="brackets">)</span>
+            </div>
+            <br />
+            <div>
+              <span class="comment">//set 能保證 裡面的東西 不重複</span>
+            </div>
+            <div>
+              <span class="comment">//添加重複元素時 他並不會被添加進去 </span>
+            </div>
+            <div>
+              <span class="variable">set</span>
+              <span class="variable">.add</span>
+              <span class="brackets">(</span> <span class="number">1</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="word">console</span>
+              <span class="variable">.log</span>
+              <span class="brackets">(</span> <span class="variable">set</span>
+              <span class="brackets">) </span>
+              <span class="comment">//{ 1,2,3,7 } //仍然只有 1 個 1</span>
+            </div>
+            <br />
+            <div>
+              <span class="comment"
+                >//.delete()刪除方法,返回是否有此元素並刪除是否成功(true)</span
+              >
+            </div>
+            <div>
+              <span class="variable">set</span>
+              <span class="variable">.delete</span>
+              <span class="brackets">(</span> <span class="number">1</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="word">console</span>
+              <span class="variable">.log</span>
+              <span class="brackets">(</span> <span class="variable">set</span>
+              <span class="brackets">) </span>
+              <span class="comment">//{ 2,3,7 } </span>
+            </div>
+            <br />
+            <div>
+              <span class="comment"
+                >//.has()查詢是否有此元素的方法,返回是否有此元素(true)</span
+              >
+            </div>
+            <div>
+              <span class="variable">set.has</span>
+              <span class="brackets">(</span> <span class="number">2</span>
+              <span class="brackets">)</span>
+              <span class="comment">//true</span>
+            </div>
+            <br />
+            <div>
+              <span class="comment"
+                >//.size判斷set裡有多少個元素,返回多少個元素</span
+              >
+            </div>
+            <div>
+              <span class="variable">set.size</span>
+              <span class="comment">// 3</span>
+            </div>
+            <br />
+            <div>
+              <span class="comment">//遍歷可以用 forEach()</span>
+            </div>
+            <div>
+              <span class="variable">set.forEach</span>
+              <span class="brackets">(</span> <span class="word">number</span>
+              <span class="function">=> </span>
+              <span class="word">console</span>
+              <span class="variable">.log</span>
+              <span class="brackets">(</span> <span class="word">number</span>
+              <span class="brackets">))</span>
+            </div>
+          </div>
+          <div class="articleCardTitle cor36 fz34">map</div>
+
+          <p class="fz28">map 很像 object 也是 key value 配對的形式</p>
+          <div class="bgcVS">
+            <p>先 new 一個 map</p>
+            <div>
+              <span class="function">const </span>
+              <span class="word">person</span> <span class="then">= new </span>
+              <span class="word">Map</span> <span class="brackets">()</span>
+            </div>
+            <br />
+            <div><span class="comment">//往裡面添加東西用.set()</span></div>
+            <div>
+              <span class="word">person</span>
+              <span class="variable">.set</span>
+              <span class="brackets">(</span> <span class="src">"name"</span>
+              <span class="then">,</span> <span class="src">"wayne"</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="word">person</span>
+              <span class="variable">.set</span>
+              <span class="brackets">(</span> <span class="src">"age"</span>
+              <span class="then">,</span> <span class="number">18</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="word">person</span>
+              <span class="variable">.set</span>
+              <span class="brackets">(</span> <span class="src">"hobby"</span>
+              <span class="then">,</span> <span class="brackets">[</span>
+              <span class="src">"leetcode"</span> <span class="then">,</span>
+              <span class="src">"sleep"</span> <span class="brackets">])</span>
+            </div>
+            <div>
+              <span class="word">console</span>
+              <span class="variable">.log</span>
+              <span class="brackets">(</span> <span class="word">person</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="comment"
+                >//Map(3){"name"=>"wayne","age"=>18,"hobby"=>Array(2)}</span
+              >
+            </div>
+            <br />
+            <div>
+              <span class="comment">//get()方法,可以用key獲取value的值</span>
+            </div>
+            <div>
+              <span class="word">console</span>
+              <span class="variable">.log</span>
+              <span class="brackets">(</span> <span class="word">person</span>
+              <span class="variable">.get</span>
+              <span class="brackets">(</span> <span class="src">"name"</span>
+              <span class="brackets">))</span>
+              <span class="comment">//"wayne"</span>
+            </div>
+            <br />
+            <div>
+              <span class="comment"
+                >//.size判斷map裡有多少個元素,返回多少個元素</span
+              >
+            </div>
+            <div>
+              <span class="variable">map.size</span>
+              <span class="comment">// 3</span>
+            </div>
+            <br />
+            <div>
+              <span class="comment">//has()方法,返回有無這個key</span>
+            </div>
+            <div>
+              <span class="word">person</span> <span class="word">.has</span>
+              <span class="brackets">(</span> <span class="src">"age"</span>
+              <span class="brackets">)</span>
+              <span class="comment">//true</span>
+            </div>
+            <br />
+            <div>
+              <span class="comment"
+                >//已經有age再添加age,會用新的值去更新舊值</span
+              >
+            </div>
+            <div>
+              <span class="word">person</span>
+              <span class="variable">.set</span>
+              <span class="brackets">(</span> <span class="src">"age"</span>
+              <span class="then">,</span> <span class="number">30</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="word">console</span>
+              <span class="variable">.log</span>
+              <span class="brackets">(</span> <span class="word">person</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="comment"
+                >//Map(3){"name"=>"wayne","age"=>30,"hobby"=>Array(2)}</span
+              >
+            </div>
+            <br />
+            <div>
+              <span class="comment"
+                >//.delete()刪除方法,返回是否有此元素並刪除是否成功(true)</span
+              >
+            </div>
+            <div>
+              <span class="word">person</span>
+              <span class="variable">.delete</span>
+              <span class="brackets">(</span> <span class="src">"age"</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="word">console</span>
+              <span class="variable">.log</span>
+              <span class="brackets">(</span> <span class="word">person</span>
+              <span class="brackets">)</span>
+            </div>
+            <div>
+              <span class="comment"
+                >//Map(3){"name"=>"wayne","hobby"=>Array(2)}</span
+              >
+            </div>
+            <br />
+          </div>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <div class="articleCardTitle cor36 fz34"></div>
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <div class="articleCardTitle cor36 fz34"></div>
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <p class="fz28"></p>
+          <br /><br />
+          <div class="articleCardTitle cor36 fz34"></div>
+          <p class="fz28"></p>
+          <br /><br />
         </div>
       </div>
     </div>
